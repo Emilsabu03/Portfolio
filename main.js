a29b08c638eadb5e81050679fda97b261ca6599f
Emilsabu03 <Emil.Sabu@edu.sait.ca>
2024-11-15 20:54:18 -0700

    New Javascript

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-console.log(document.querySelector(".home-info h3").textContent);
-
 window.onload = function() {
     const greeting = document.querySelector(".home-info h3");  
     const currentHour = new Date().getHours();  
     let greetingText = "Hi, I'm a Graphic Designer"; 
+    let greetingColor = ""; // Variable for dynamic color
 
     if (currentHour < 12) {
         greetingText = "Good Morning, I'm a Graphic Designer";
-        greeting.classList.add("morning")  
+        greetingColor = "#ff9900"; // Color for morning
     } else if (currentHour < 18) {
         greetingText = "Good Afternoon, I'm a Graphic Designer"; 
-        greeting.classList.add("afternoon")
+        greetingColor = "#55e1fa"; // Color for afternoon
     } else {
         greetingText = "Good Evening, I'm a Graphic Designer";
-        greeting.classList.add("evening")  
+        greetingColor = "#270dd4"; // Color for evening
     }
 
-
     greeting.textContent = greetingText;
+    greeting.style.color = greetingColor; 
 
     console.log(greeting.textContent);
 };
-
 
 const socialLinks = document.querySelectorAll('.social a');
 
